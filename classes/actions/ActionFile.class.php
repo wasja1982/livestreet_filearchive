19dbf98f08c8bbbd32379502b9ee743f73473a36
wasja1982 <wasja@yandex.ru>
2014-08-10 13:15:50 +0300

    Версия 1.0.3

--- classes/actions/ActionFile.class.php
+++ classes/actions/ActionFile.class.php
@@ -2,7 +2,7 @@
 /**
  * File Archive - тип топика "файл"
  *
- * Версия:	1.0.2
+ * Версия:	1.0.3
  * Автор:	Александр Вереник
  * Профиль:	http://livestreet.ru/profile/Wasja/
  * GitHub:	https://github.com/wasja1982/livestreet_filearchive
